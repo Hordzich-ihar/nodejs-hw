6860f0dd13ec4a85bb4335458e14e4ce44bb9cc0
Ihar <Garik666770@gmail.com>
2026-05-24 16:45:54 +0300

    04-auth

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import { authRouter } from './routes/authRoutes.js';
 import { notesRouter } from './routes/notesRoutes.js';
 
 const app = express();
@@ -14,8 +16,15 @@ const PORT = Number(process.env.PORT) || 3000;
 
 app.use(logger);
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
 
+app.use(authRouter);
 app.use(notesRouter);
 
 app.use(errors());
